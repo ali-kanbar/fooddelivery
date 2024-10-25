6ec1c1cdc19ad4b27a436014943d7f388b0b4c03
Ali Kanbar <ali-kanbar@outlook.com>
2024-10-25 11:49:39 +0300

    Creating mongoose

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import { connectDB } from './config/db.js';
+import foodRouter from './routes/foodRoute.js';
+
+const app = express();
+const port = 4000
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(cors())
+
+//DB CONNECTION
+connectDB();
+
+//Api endpoints
+app.use("/api/food",foodRouter)
+app.use("/images",express.static("uploads"))
+
+app.get("/",(req,res)=>{
+    res.send("Api working")
+})
+
+app.listen(port,()=>{
+    console.log(`Server is listening on http://localhost:${port}`)
+})
+
+//mongodb+srv://alikanbar:alikan20ali20@cluster0.qdyfh.mongodb.net/?
