9d5acd3122ea9aa7ab5600c79c2ce976e2ddd942
Ali Kanbar <ali-kanbar@outlook.com>
2024-10-25 23:29:53 +0300

    Creating the admin page

---
+++ admin/src/pages/Add/Add.jsx
@@ -0,0 +1,133 @@
+import React, { useEffect, useState } from "react";
+import "./Add.css";
+import { assets } from "../../assets/assets";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+const Add = () => {
+  const [image, setImage] = useState(false);
+  const [data, setData] = useState({
+    name: "",
+    description: "",
+    price: "",
+    category: "Salad",
+  });
+
+  const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData((data) => ({ ...data, [name]: value }));
+  };
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("name", data.name);
+    formData.append("description", data.description);
+    formData.append("price", Number(data.price));
+    formData.append("category", data.category);
+    formData.append("image", image);
+    const response = await axios.post(
+      "http://localhost:4000/api/food/add",
+      formData
+    );
+    if (response.data.success) {
+      setData({
+        name: "",
+        description: "",
+        price: "",
+        category: "Salad",
+      });
+      setImage(false);
+      toast.success(response.data.message);
+    } else {
+      toast.error(response.data.message);
+    }
+  };
+
+  useEffect(() => {
+    if (image) {
+      const imageUrl = URL.createObjectURL(image);
+      return () => {
+        URL.revokeObjectURL(imageUrl); // Clean up old image URLs
+      };
+    }
+  }, [image]);
+
+  return (
+    <div className="add">
+      <form className="flex-col" onSubmit={onSubmitHandler}>
+        <div className="add-img-upload flex-col">
+          <p>Upload Image</p>
+          <label htmlFor="image">
+            <img
+              src={image ? URL.createObjectURL(image) : assets.upload_area}
+            />
+          </label>
+          <input
+            onChange={(e) => setImage(e.target.files[0])}
+            type="file"
+            id="image"
+            hidden
+            required
+          ></input>
+        </div>
+        <div className="add-product-name flex-col">
+          <p>Product name</p>
+          <input
+            onChange={onChangeHandler}
+            value={data.name}
+            type="text"
+            name="name"
+            placeholder="Type Here"
+          />
+        </div>
+        <div className="add-product-description flex-col">
+          <p>Product description</p>
+          <textarea
+            name="description"
+            placeholder="Write content here"
+            rows="6"
+            onChange={onChangeHandler}
+            value={data.description}
+            required
+          />
+        </div>
+        <div className="add-category-price">
+          <div className="add-category flex-col">
+            <p>Product category</p>
+            <select
+              name="category"
+              onChange={onChangeHandler}
+              value={data.category}
+            >
+              <option value="Salad">Salad</option>
+              <option value="Rolls">Rolls</option>
+              <option value="Deserts">Deserts</option>
+              <option value="Sandwich">Sandwich</option>
+              <option value="Cake">Cake</option>
+              <option value="Pure Veg">Pure Veg</option>
+              <option value="Pasta">Pasta</option>
+              <option value="Noodles">Noodles</option>
+            </select>
+          </div>
+          <div className="add-price flex-col">
+            <p>Prodct Price</p>
+            <input
+              type="Number"
+              name="price"
+              placeholder="$20"
+              onChange={onChangeHandler}
+              value={data.price}
+            />
+          </div>
+        </div>
+        <button type="submit" className="add-btn">
+          Add
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Add;
--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -12,33 +12,33 @@ const addFood = async (req, res) => {
   });
   try {
     await food.save();
-    res.json({ succes: true, message: "Food Added" });
+    res.json({ success: true, message: "Food Added" });
   } catch (error) {
     console.log(error);
-    res.json({ succes: false, message: "Error" });
+    res.json({ success: false, message: "Error" });
   }
 };
 
 const listFood = async (req, res) => {
   try {
     const foods = await foodModel.find();
-    res.json({ succes: true, data: foods });
+    res.json({ success: true, data: foods });
   } catch (error) {
     console.log(error);
-    res.json({ succes: false, message: "Error" });
+    res.json({ success: false, message: "Error" });
   }
 };
 
-const removeFood = async (req,res) => {
-    try {
-        const food = await foodModel.findById(req.body.id)
-        fs.unlink(`uploads/${food.image}`,()=>{})
-        await foodModel.findByIdAndDelete(req.body.id)
-        res.json({ succes: true, message: "Food Removed" });
-    } catch (error) { 
-        console.log(error);
-        res.json({ succes: false, message: "Error" });
-    }
-}
+const removeFood = async (req, res) => {
+  try {
+    const food = await foodModel.findById(req.body.id);
+    fs.unlink(`uploads/${food.image}`, () => {});
+    await foodModel.findByIdAndDelete(req.body.id);
+    res.json({ success: true, message: "Food Removed" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
 
-export { addFood, listFood, removeFood};
+export { addFood, listFood, removeFood };
