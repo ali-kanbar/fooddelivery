0aaf6b5aeb0ba9b2ca1889973f40c48a422093e7
Ali Kanbar <ali-kanbar@outlook.com>
2024-10-26 23:03:46 +0300

    Linking cart and database

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,22 +1,38 @@
 import { createContext, useEffect, useState } from "react";
-import { food_list } from "../assets/assets";
+import axios from "axios";
 
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const [food_list, setFoodList] = useState([]);
   const [token, setToken] = useState("");
-  const addToCart = (itemId) => {
+  const url = "http://localhost:4000";
+
+  const addToCart = async (itemId) => {
     if (!cartItems[itemId]) {
       setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
     } else {
       setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     }
+    if (token) {
+      await axios.post(
+        url + "/api/cart/add",
+        { itemId },
+        { headers: { token } }
+      );
+    }
   };
 
-  const removeFromCart = (itemId) => {
+  const removeFromCart = async (itemId) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+    if (token) {
+      await axios.post(
+        url + "/api/cart/remove",
+        { itemId },
+        { headers: { token } }
+      );
+    }
   };
 
   const getTotalCartAmount = () => {
@@ -29,11 +45,28 @@ const StoreContextProvider = (props) => {
     }
     return totalAmount;
   };
+
+  const loadFoodList = async () => {
+    const response = await axios.get(`${url}/api/food/list`);
+    setFoodList(response.data.data);
+  };
+
+  const loadCartData = async (token) => {
+    const response = await axios.post(url+"/api/cart/get",{},{headers:{token}})
+    setCartItems(response.data.cartData);
+  }
+
   useEffect(() => {
-    if (localStorage.getItem("token")) {
-      setToken(localStorage.getItem("token"));
+    async function loadData() {
+      await loadFoodList();
+      if (localStorage.getItem("token")) {
+        setToken(localStorage.getItem("token"));
+        await loadCartData(localStorage.getItem("token"))
+      }
     }
+    loadData()
   }, []);
+
   const contextValue = {
     food_list,
     cartItems,
--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -4,7 +4,7 @@ import { StoreContext } from "../../context/StoreContext";
 import CartTotal from "../../components/CartTotal/CartTotal";
 
 const Cart = () => {
-  const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
+  const { cartItems, food_list, removeFromCart, url } = useContext(StoreContext);
   
   return (
     <div className="cart">
@@ -24,7 +24,7 @@ const Cart = () => {
             return (
               <div>
                 <div className="cart-items-title cart-items-item">
-                  <img src={item.image} alt="" />
+                  <img src={url+"/images/"+item.image} alt="" />
                   <p>{item.name}</p>
                   <p>${item.price}</p>
                   <p>{cartItems[item._id]}</p>
