cb92c9f51286be3193e86641b9ccd182198223ca
Ali Kanbar <155682302+ali-kanbar@users.noreply.github.com>
2024-11-01 16:07:46 +0200

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://foodDelivery:X6xAuPAlyIjenZAo@cluster0.zo93s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>console.log("DB connection established"))
+    await mongoose.connect("mongodb+srv://foodDelivery:X6xAuPAlyIjenZAo@cluster0.zo93s.mongodb.net/food-delivery").then(()=>console.log("DB connection established"))
 }
